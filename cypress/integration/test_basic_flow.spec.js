89e46e0b0737dc7876ab3b73c7b191236d141fe2
Srinidhi <s.hebbar93@gmail.com>
2021-09-02 02:51:37 +0530

    Added extra wait after login

--- cypress/integration/test_basic_flow.spec.js
+++ cypress/integration/test_basic_flow.spec.js
@@ -6,13 +6,13 @@ describe("Testing basic validation", () => {
     cy.findByRole("textbox", { name: /email/i }).type("mp.shebbar@gmail.com");
     cy.findByLabelText(/password/i).type("johndoe123");
     cy.findByRole("button", { name: /login/i }).click();
-    cy.wait(2000);
+    cy.wait(10000);
 
     cy.get(
       ":nth-child(2) > .side-nav__main__item__icon > :nth-child(1) > button"
     ).click();
     cy.findByRole("button", { name: /new project/i }).click();
-    cy.wait(10000);
+    cy.wait(2000);
 
     const projectName = uuidv4().substring(0, 20);
 
